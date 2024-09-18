41380c5fe1c804230ca5c20bde8c7078eeff8b30
Kelvinbd11 <newkelvinbd@gmail.com>
2024-09-18 17:41:19 -0300

    feat:logo-navegador

--- website-projetacao/src/App.jsx
+++ website-projetacao/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
@@ -10,24 +9,22 @@ import Ventiloar from './pages/Ventiloar/Ventiloar'
 import Peneiras from './pages/Peneiras/Peneiras'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div className='App'>
       <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/invest" element={<Invest />} />
-              <Route path="/blog" element={<Blog />} />
-              <Route path="/Projetacao" element={<Projetacao />} />
-              <Route path="/Ventiloar" element={<Ventiloar />} />
-              <Route path="/Peneiras" element={<Peneiras />} />
-
-            </Routes>
-          </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/invest" element={<Invest />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/Projetacao" element={<Projetacao />} />
+          <Route path="/Ventiloar" element={<Ventiloar />} />
+          <Route path="/Peneiras" element={<Peneiras />} />
+        </Routes>
+      </BrowserRouter>
       </div>
 
     </>
